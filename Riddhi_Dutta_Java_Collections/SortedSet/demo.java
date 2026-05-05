b16c6602d5f4dfd3155cc7f49d995cdad187fada
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2026-05-05 18:11:26 +0530

    add: demo for TreeSet

--- Riddhi_Dutta_Java_Collections/SortedSet/demo.java
+++
@@ -1,7 +0,0 @@
-package LLD_Java_Essentials.Riddhi_Dutta_Java_Collections.SortedSet;
-
-public class demo {
-    public static void main(String[] args) {
-        System.out.println("\n SortedSet demo");
-    }
-}
---
+++ Riddhi_Dutta_Java_Collections/TreeSet/demo.java
@@ -0,0 +1,26 @@
+package LLD_Java_Essentials.Riddhi_Dutta_Java_Collections.TreeSet;
+
+import java.util.NavigableSet;
+import java.util.TreeSet;
+
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("\n TreeSet demo");
+
+        // Declaring as NavigableSet interface instead of Set, as floor(), ceil() etc are part of NavigableSet interface
+        NavigableSet<Integer> st = new TreeSet<>(); 
+        st.add(1);
+        st.add(1);
+        st.add(2);
+        st.add(3);
+
+        for(int x: st)
+            System.out.println(x);
+
+        System.out.println("Floor of 1 (Lesser than or equal to) => "+st.floor(1));
+        System.out.println("Lower of 1 (Lesser than) => "+st.lower(1)); 
+
+        System.out.println("Ceil of 1 (Greater than or equal to; lower_bound) => "+st.ceiling(1));
+        System.out.println("Higher of 1 (Greater than; upper_bound) => "+st.higher(1));
+    }
+}
