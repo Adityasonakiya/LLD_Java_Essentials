da50700c86bbc8756e4b9066995ad7171c846410
Shivam Shukla <67953167+Lastropy@users.noreply.github.com>
2026-05-12 20:56:59 +0530

    patch: added synchronized function

--- Riddhi_Dutta_Java_Multithreading/Synchronization/solution.java
+++ Riddhi_Dutta_Java_Multithreading/Synchronization/solution.java
@@ -19,41 +19,58 @@ class ThreadSafeStack {
         return top == -1;
     }
 
-    boolean add(int ele){
-        // In Java, ANY OBJECT can be used as a lock
-        // So, synchronized(new String("ss")) is valid
-        synchronized(lock) {
-            if(isFull()) {
-                System.out.println("Can't add; Stack full");
-                return false;
-            }
-            top = top + 1;
-            try {
-                Thread.sleep(1000);
-            } catch (Exception e) {
-                System.out.println("Exception during simulateSleep(): "+e);
-            }
-            a[top] = ele;
-            return true;
+    // boolean add(int ele){
+    //     // In Java, ANY OBJECT INSTANCE can be used as a lock
+    //     // So, synchronized(new String("ss")) is valid
+    //     // What if you're inside a static fn? Use synchronized(ClassName.class) 
+    //     synchronized(lock) {
+    //        ...
+    //     }
+    // }
+
+    // int remove(){
+    //     synchronized(lock) {
+    //      ...
+    //     }
+    // }
+
+    synchronized boolean add(int ele){
+        // In background, java basically wraps everything in sychronized(this)
+        // i.e. current class instance is used as lock!! 
+        // So now, for all synchronized methods in the class, they have same lock
+        // which means, if multiple threads are calling multiple methods of this class instance, 
+        // only 1 thread can execute all synchronized methods at a time.
+
+        // Note - Since internally, static methods use ClassName.class as lock and non-static methods use this,
+        // so, due to seperate locks, threads can enter static and non-static methods at the same time.
+        if(isFull()) {
+            System.out.println("Can't add; Stack full");
+            return false;
         }
+        top = top + 1;
+        try {
+            Thread.sleep(1000);
+        } catch (Exception e) {
+            System.out.println("Exception during simulateSleep(): "+e);
+        }
+        a[top] = ele;
+        return true;
     }
 
-    int remove(){
-        synchronized(lock) {
-            if(isEmpty()){
-                System.out.println("Stack Empty");
-                return Integer.MIN_VALUE;
-            }
-            int val = a[top];
-            a[top] = Integer.MIN_VALUE;
-            try {
-                Thread.sleep(1000);
-            } catch (Exception e) {
-                System.out.println("Exception during simulateSleep(): "+e);
-            }
-            top = top -1;
-            return val;
+    synchronized int remove(){
+        if(isEmpty()){
+            System.out.println("Stack Empty");
+            return Integer.MIN_VALUE;
         }
+        int val = a[top];
+        a[top] = Integer.MIN_VALUE;
+        try {
+            Thread.sleep(1000);
+        } catch (Exception e) {
+            System.out.println("Exception during simulateSleep(): "+e);
+        }
+        top = top -1;
+        return val;
     }
 }
 
